34290edbf0eeec6a53886928f7b41127e4ff5452
6epotato <aarifurrahman93@gmail.com>
2022-08-03 17:34:01 +0600

    concected to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const express=require('express')
-const app=express()
-const cors=require('cors')
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const cors = require('cors')
 app.use(cors())
+app.use(express.json())
 
 
-const port=process.env.PORT || 4000
+const port = process.env.PORT || 4000
 
-app.get('/',(req,res)=>{
+
+// username: digi_money1
+// password: 0cmJopaqLVn7snJi
+
+
+
+const uri = "mongodb+srv://digi_money1:0cmJopaqLVn7snJi@cluster0.928uo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
     res.send("digimoney server start successfully")
 })
 
 
 
 
-app.listen(port,()=>console.log("Run successfull"))
+app.listen(port, () => console.log("Run successfull"))
