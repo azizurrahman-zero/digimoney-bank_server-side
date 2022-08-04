5bf09a2d76610d7dc73bdb597ba569511e6d7bef
6epotato <aarifurrahman93@gmail.com>
2022-08-04 17:37:26 +0600

    change mongodb exixst function

--- index.js
+++ index.js
@@ -16,14 +16,10 @@ const port = process.env.PORT || 4000
 
 const uri = "mongodb+srv://digi_money1:0cmJopaqLVn7snJi@cluster0.928uo.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+
 async function run() {
-    try{
+    try {
         await client.connect();
         const usersCollection = client.db("digi_money1").collection("users");
 
@@ -33,22 +29,22 @@ async function run() {
             const cursor = usersCollection.find(query);
             const users = await cursor.toArray();
             res.send(users);
-          });
+        });
 
-          app.put("/user/:email", async (req, res) => {
+        app.put("/user/:email", async (req, res) => {
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
             const options = { upsert: true };
             const updateDoc = {
-              $set: user,
+                $set: user,
             };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
-            
-            res.send({ result});
-          });
-    }finally {
-  }
+
+            res.send({ result });
+        });
+    } finally {
+    }
 }
 run().catch(console.dir);
 
