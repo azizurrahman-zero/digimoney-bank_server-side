faa88ef27066448f8c594c46ff7c2bcccc298500
6epotato <aarifurrahman93@gmail.com>
2022-08-05 23:53:00 +0600

    delete api done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
 app.use(cors())
@@ -46,12 +46,12 @@ async function run() {
         });
 
         // delete from users a user
-        // app.delete('/users/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await usersCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
 
         app.get('/approvedUsers', async (req, res) => {
             const query = {};
