4a4866d90a7a79f6a33c86d675b7e444d181c230
Istiak <istieak.hasan1996@gmail.com>
2022-08-15 22:44:55 +0600

    balance tranfer api created

--- index.js
+++ index.js
@@ -19,173 +19,33 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  try {
+    await client.connect();
+    const usersCollection = client.db("digi_money1").collection("users");
+    const approvedUsersCollection = client
+      .db("digi_money1")
+      .collection("approvedUsers");
+    const usersReviewCollection = client
+      .db("digi_money1")
+      .collection("reviews");
+    const transectionCollection = client
+      .db("digi_money1")
+      .collection("transection");
 
-    try {
-        await client.connect();
-        const usersCollection = client.db("digi_money1").collection("users");
-        const approvedUsersCollection = client.db("digi_money1").collection("approvedUsers");
-        const usersReviewCollection = client.db("digi_money1").collection("reviews");
-    //    create new user and save the user data to database 
-        app.post("/adduser", async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-            console.log(result)
-          });
+    //    create new user and save the user data to database
+    app.post("/adduser", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+      console.log(result);
+    });
 
-          app.get("/users", async (req, res) => {
-            const query = {};
-            const cursor = usersCollection.find(query);
-            const users = await cursor.toArray();
-            res.send(users);
-        });
-
-        app.put("/user/:email", async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: user,
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc, options);
-
-            res.send({ result });
-        });
-
-        // delete from users a user
-        app.delete('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id)
-            const query = { _id: ObjectId(id) };
-            const result = await usersCollection.deleteOne(query);
-            res.send(result);
-        })
-
-          
-
-        app.get('/approvedUsers', async (req, res) => {
-            const query = {};
-            const cursor = approvedUsersCollection.find(query);
-            const users = await cursor.toArray();
-            res.send(users);
-        })
-
-
-    //    // find out user by email
-    //    app.get('/finduser', async(req,res)=>{
-    //     const email=req.query.email;
-    //     const query= {email:email}
-    //     console.log('email',email);
-    //     console.log('query',query);
-    //     const result= await approvedUsersCollection.findOne(query);
-    //     console.log("result", result);
-    //     res.send(result)
-
-    // })
-    app.get('/finduser', async(req,res)=>{
-
-        const email = req.query.email;
-
-      const result = await approvedUsersCollection.findOne({ email: email });
-      if (!result) {
-        res.send({ find: false });
-        return;
-      } else {
-        res.send(result);
-      }
-    })
-      
-
-        // get the user from search option 
-        // app.get('/search/:key', async (req, res) => {
-        //     let result = await approvedUsersCollection.find({
-        //         "$or": [
-        //             {
-        //                 displayName: { $regex: req.params.key }
-        //             },
-        //             {
-        //                 email: { $regex: req.params.key }
-        //             }
-        //         ]
-        //     })
-        //     res.send(result)
-        // })
-
-
-        // adding account number
-        app.patch("/accountNumber/:id", async (req, res) => {
-          const id = req.params.id;
-          const data = req.body;
-          const query = { _id: ObjectId(id) };
-          console.log(query);
-          console.log(data);
-          const update = {
-            $set: {
-              accountNumber:data.accountNumber
-            },
-          };
-          const result = await usersCollection.updateOne(query, update);
-          res.send(result);
-        });
-
-        // post approved users
-        app.post('/approvedUsers', async (req, res) => {
-            const newUser = req.body;
-         
-             const result = await approvedUsersCollection.insertOne(newUser)
-             console.log(result)
-             res.send(result);
-
-        })
- 
-    //    update amount 
-    // app.patch('/approvedUsers/:id', async(req,res)=>{
-    //     const id = req.params.id;
-    //     const updatedAmount = req.body;
-    //     const filter= {_id:ObjectId(id)};
-    //     const updatedDoc ={
-    //         $set:{
-    //             amount:updatedAmount.amount
-    //         }
-    //     }
-       
-    //     const result = await approvedUsersCollection.updateOne(filter,updatedDoc)
-    //     console.log("result",result);
-    //     res.send(result)
-
-    // })
-        // update ammount
-    app.patch("/approvedUsers/:id", async (req, res) => {
-        const id = req.params.id;
-        const updatedAmount = req.body;
-        const query = { _id: id };
-        const update = {
-          $set: {
-            amount:updatedAmount.amount
-          },
-        };
-        const result = await approvedUsersCollection.updateOne(query, update);
-        res.send(result);
-      });
-
-      
-          app.put("/approvedUsers/admin/:email", async (req, res) => {
-            const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-              $set: { role: "admin" },
-            };
-            const result = await approvedUsersCollection.updateOne(filter, updateDoc);
-            res.send(result);
-          });
-          
-          app.get("/approvedUser/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: id };
-            const user = await approvedUsersCollection.findOne(query);
-            res.send(user);
-          });
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
 
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -204,13 +64,106 @@ async function run() {
       res.send({ result });
     });
 
-    // // delete from request user database
-    // app.delete('/users/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = { _id: ObjectId(id) };
-    //     const result = await usersCollection.deleteOne(query);
-    //     res.send(result);
-    // })
+    // delete from users a user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/approvedUsers", async (req, res) => {
+      const query = {};
+      const cursor = approvedUsersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.get("/finduser", async (req, res) => {
+      const email = req.query.email;
+
+      const result = await approvedUsersCollection.findOne({ email: email });
+      if (!result) {
+        res.send({ find: false });
+        return;
+      } else {
+        res.send(result);
+      }
+    });
+
+    // adding account number
+    app.patch("/accountNumber/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: ObjectId(id) };
+      console.log(query);
+      console.log(data);
+      const update = {
+        $set: {
+          accountNumber: data.accountNumber,
+        },
+      };
+      const result = await usersCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // post approved users
+    app.post("/approvedUsers", async (req, res) => {
+      const newUser = req.body;
+
+      const result = await approvedUsersCollection.insertOne(newUser);
+      console.log(result);
+      res.send(result);
+    });
+
+    // update ammount
+    app.patch("/approvedUsers/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedAmount = req.body;
+      const query = { _id: id };
+      const update = {
+        $set: {
+          amount: updatedAmount.amount,
+        },
+      };
+      const result = await approvedUsersCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.put("/approvedUsers/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await approvedUsersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.get("/approvedUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const user = await approvedUsersCollection.findOne(query);
+      res.send(user);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+
+      res.send({ result });
+    });
 
     // delete from users a user
     app.delete("/users/:id", async (req, res) => {
@@ -221,27 +174,15 @@ async function run() {
       res.send(result);
     });
 
-
-        // post approved users
-        app.post('/approvedUsers', async (req, res) => {
-            const newUser = req.body;
-            console.log(newUser)
-             const result = await approvedUsersCollection.insertOne(newUser)
-             console.log(result)
-             res.send(result);
-
-        })
-        // Get a single user information 
-        app.patch('/user',async(req,res)=>{
-             const email=req.query.email
-             const query={email:email} 
-             const data=req.body
-             console.log(data)
-             const find= await usersCollection.findOne(query)
-             const newBalance=parseFloat(find.amount)+ 2
-             console.log(newBalance)
-             console.log("this is email",email)
-        })
+    // post approved users
+    app.post("/approvedUsers", async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await approvedUsersCollection.insertOne(newUser);
+      console.log(result);
+      res.send(result);
+    });
+    // Get a single user information
 
     app.get("/approvedUsers", async (req, res) => {
       const query = {};
@@ -250,15 +191,12 @@ async function run() {
       res.send(users);
     });
 
-
-
     // post approved users
     app.post("/approvedUser", async (req, res) => {
       const newUser = req.body;
       const result = await approvedUsersCollection.insertOne(newUser);
       res.send(result);
     });
-
 
     app.delete("/approvedUser/:id", async (req, res) => {
       const id = req.params.id;
@@ -284,19 +222,17 @@ async function run() {
       res.send(user);
     });
 
-
     // ===============================================customer review ============================================//
-    // Insert review data  to database 
-    app.post("/review",async(req,res)=>{
-      const review=req.body
-      const result=await usersReviewCollection.insertOne(review)
-      res.send(result)
-      
-    })
-    //get all review data from database 
-    app.get("/review",async(req,res)=>{
-      const result=await usersReviewCollection.find({}).toArray()
-      res.send(result)
+    // Insert review data  to database
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await usersReviewCollection.insertOne(review);
+      res.send(result);
+    });
+    //get all review data from database
+    app.get("/review", async (req, res) => {
+      const result = await usersReviewCollection.find({}).toArray();
+      res.send(result);
     });
     // ==================================================Check admin ============================================//
     app.get("/admin", async (req, res) => {
@@ -307,6 +243,47 @@ async function run() {
         return res.send({ admin: isAdmin });
       } else {
         return res.send({ admin: false });
+      }
+    });
+
+    // ==============================================Transfer Balance==========================================//
+    app.patch("/transfer", async (req, res) => {
+      const { accountNumber, amount } = req.body;
+      const email = req.query.email;
+      const receiverinfoquery = { accountNumber: accountNumber };
+      const senderinfoquery = { email: email };
+      const findTargetedaccount = await approvedUsersCollection.findOne(
+        receiverinfoquery
+      );
+      const updateAmount =
+        parseFloat(findTargetedaccount.amount) + parseFloat(amount);
+      const update = {
+        $set: {
+          amount: updateAmount,
+        },
+      };
+      const transferBalance = await approvedUsersCollection.updateOne(
+        receiverinfoquery,
+        update
+      );
+      if (transferBalance.modifiedCount) {
+        const findSenderInfo = await approvedUsersCollection.findOne(
+          senderinfoquery
+        );
+        console.log(findSenderInfo.amount, "lol", amount);
+        const updateSenderAmount =
+          parseFloat(findSenderInfo.amount) - parseFloat(amount);
+        console.log(updateSenderAmount);
+        const updatesender = {
+          $set: {
+            amount: updateSenderAmount,
+          },
+        };
+        const finalResult = await approvedUsersCollection.updateOne(
+          senderinfoquery,
+          updatesender
+        );
+        res.send(finalResult);
       }
     });
   } finally {
